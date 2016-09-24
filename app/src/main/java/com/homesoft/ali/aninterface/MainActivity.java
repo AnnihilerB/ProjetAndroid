ee1c570e8ffe89448928e7b00761140a275b368a
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-09-24 12:53:11 +0200

    Ajout d'une instance des traitements, Refactoring des boutons pour correspondre à des traitements.

--- app/src/main/java/com/homesoft/ali/aninterface/MainActivity.java
+++ app/src/main/java/com/homesoft/ali/aninterface/MainActivity.java
@@ -1,7 +1,9 @@
 package com.homesoft.ali.aninterface;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,19 +16,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     ImageView logo;
     TextView info;
+    Traitements t;
+    DisplayMetrics metrics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        metrics = getResources().getDisplayMetrics();
+
         this.logo = (ImageView)findViewById(R.id.imageView);
         logo.setVisibility(View.INVISIBLE);
 
         this.info = (TextView)findViewById(R.id.textView);
 
-        Button action1 = (Button)findViewById(R.id.action1);
-        Button action2 = (Button)findViewById(R.id.action2);
+        Button action1 = (Button)findViewById(R.id.niveaugris);
+        Button action2 = (Button)findViewById(R.id.sepia);
         Button afficherCacher = (Button)findViewById(R.id.afficherCacher);
 
         info.setText("Aucune image");
@@ -34,12 +40,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         action1.setOnClickListener(this);
         action2.setOnClickListener(this);
         afficherCacher.setOnClickListener(this);
+
+        this.t = new Traitements(logo);
     }
 
     public void onClick(View view){
-        if (view.getId() == R.id.action1)
-            Toast.makeText(this, "Action 1", Toast.LENGTH_SHORT).show();
-        if (view.getId() == R.id.action2)
+        if (view.getId() == R.id.niveaugris) {
+            t.niveauxGris();
+            logo.setVisibility(View.INVISIBLE);
+            logo.setVisibility(View.VISIBLE);
+        }
+        if (view.getId() == R.id.sepia)
             Toast.makeText(this, "Action 2", Toast.LENGTH_SHORT).show();
         if (view.getId() == R.id.afficherCacher)
             afficherCacher(logo, info);
@@ -50,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             logo.setVisibility(View.VISIBLE);
             info.setText("Largeur : " + logo.getDrawable().getIntrinsicWidth() + " hauteur : " +logo.getDrawable().getIntrinsicHeight() );
             imageEstAffichée = true;
+            System.out.println(metrics);
         }
         else{
             logo.setVisibility(View.INVISIBLE);
