29cf9ee67300d10631d15af8026f394ce88d172e
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-09-25 14:25:06 +0200

    Implémentation du sépia !

--- app/src/main/java/com/homesoft/ali/aninterface/Traitements.java
+++ app/src/main/java/com/homesoft/ali/aninterface/Traitements.java
@@ -31,22 +31,51 @@ public class Traitements {
         int couleurPixel;
         int couleurDest;
 
-        int rouge;
-        int vert;
-        int bleu;
-
         for (int y = 0; y < hauteur; ++y){
             for (int x = 0; x < largeur; x++){
                 couleurPixel = bitmapOriginal.getPixel(x,y);
 
                 couleurDest = (int)( (0.299 * Color.red(couleurPixel)) + (0.587 * Color.green(couleurPixel)) + (0.114 * Color.blue(couleurPixel)) );
 
-                rouge = vert = bleu = couleurDest;
-                couleurDest = Color.rgb(rouge, vert, bleu);
-                imageDest.setPixel(x,y,couleurDest);
+                imageDest.setPixel(x,y,Color.rgb(couleurDest, couleurDest, couleurDest));
             }
         }
         this.iv.setImageBitmap(imageDest);
+    }
+
+    public void sepia() {
+
+        Bitmap imageDest = bitmapOriginal.copy(Bitmap.Config.ARGB_8888, true);
+
+        int hauteur = bitmapOriginal.getHeight();
+        int largeur = bitmapOriginal.getWidth();
+
+        int couleurPixel;
+
+        int rouge;
+        int vert;
+        int bleu;
+
+        for (int y = 0; y < hauteur; ++y) {
+            for (int x = 0; x < largeur; x++) {
+                couleurPixel = bitmapOriginal.getPixel(x, y);
+
+                rouge = (int) ((Color.red(couleurPixel) * .393) + (Color.green(couleurPixel) * .769) + (Color.blue(couleurPixel) * .189));
+                vert = (int) ((Color.red(couleurPixel) * .349) + (Color.green(couleurPixel) * .686) + (Color.blue(couleurPixel) * .168));
+                bleu = (int) ((Color.red(couleurPixel) * .272) + (Color.green(couleurPixel) * .534) + (Color.blue(couleurPixel) * .131));
+
+                if (rouge > 255)
+                    rouge = 255;
+                if (vert > 255)
+                    vert = 255;
+                if (bleu > 255)
+                    bleu = 255;
+
+                imageDest.setPixel(x, y, Color.rgb(rouge, vert, bleu));
+
+            }
+            this.iv.setImageBitmap(imageDest);
+        }
     }
 
     public void reinit(){
