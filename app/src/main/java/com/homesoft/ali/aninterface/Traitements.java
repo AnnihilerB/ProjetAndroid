9028eea6c9a3ad8b24c6b59852378e0e21955700
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-10-20 17:33:30 +0200

    Implémentation des deux augmentation du contraste. Les tableaux de pixels deviennent des données membres. Ajout de la LUT et de l'histogramme cumulé en donnée membre. Implémentation d'une méthode de mise à jour des tableaux et des bitmaps suite à la prise d'une photo

--- app/src/main/java/com/homesoft/ali/aninterface/Traitements.java
+++ app/src/main/java/com/homesoft/ali/aninterface/Traitements.java
@@ -3,18 +3,33 @@ package com.homesoft.ali.aninterface;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.homesoft.ali.aninterface.histogramme.Histogramme;
+import com.homesoft.ali.aninterface.histogramme.HistogrammeCumule;
 
 /**
  * Created by ali on 23/09/16.
  */
 public class Traitements {
 
+    // Garder en mémoire les tableaux de pixels plutot que les bitmaps.
+
     private Bitmap bitmapOriginal;
     private Bitmap bitmapTraite;
+
+    private int tabPixelsOriginal[];
+    private int tabPixelsTraitement[];
+
     private ImageView iv;
+
     private int largeur;
     private int hauteur;
+
+    LutRGB lut;
+
+    HistogrammeCumule histo;
 
     public Traitements(ImageView imageView){
         this.iv = imageView;
@@ -22,41 +37,49 @@ public class Traitements {
         BitmapDrawable drawable = (BitmapDrawable)iv.getDrawable();
         bitmapOriginal = drawable.getBitmap();
 
-        bitmapTraite = bitmapOriginal.copy(Bitmap.Config.ARGB_8888, true);
-
         hauteur = bitmapOriginal.getHeight();
         largeur = bitmapOriginal.getWidth();
+
+        bitmapTraite = Bitmap.createBitmap(largeur, hauteur, Bitmap.Config.ARGB_8888);
+
+        tabPixelsOriginal = new int[largeur * hauteur];
+        tabPixelsTraitement = new int[largeur * hauteur];
+
+        bitmapOriginal.getPixels(tabPixelsOriginal, 0, largeur, 0, 0, largeur, hauteur);
+
+        lut = new LutRGB();
+        lut.genererLUT(bitmapOriginal);
+
+        histo = new HistogrammeCumule();
+        histo.genererHistogramme(bitmapOriginal);
     }
 
     public void niveauxGris(){
 
-        int pixelSource[] = new int[largeur * hauteur];
-
-        bitmapOriginal.getPixels(pixelSource, 0, largeur, 0, 0, largeur, hauteur);
-
-        for (int i = 0; i < pixelSource.length; ++i){
-            int couleurGris = (int)( (0.299 * Color.red(pixelSource[i])) + (0.587 * Color.green(pixelSource[i])) + (0.114 * Color.blue(pixelSource[i])) );
-            pixelSource[i] = Color.rgb(couleurGris, couleurGris, couleurGris);
+        for (int i = 0; i < tabPixelsOriginal.length; ++i){
+            int couleurGris = (int)( (0.299 * Color.red(tabPixelsOriginal[i])) + (0.587 * Color.green(tabPixelsOriginal[i])) + (0.114 * Color.blue(tabPixelsOriginal[i])) );
+            tabPixelsTraitement[i] = Color.rgb(couleurGris, couleurGris, couleurGris);
         }
-        bitmapTraite.setPixels(pixelSource, 0, largeur, 0, 0, largeur,hauteur);
+        Log.i("l", String.valueOf(largeur));
+        Log.i("h", String.valueOf(hauteur));
+        Log.i("lbitmap", String.valueOf(bitmapOriginal.getWidth()));
+        Log.i("hbitmap", String.valueOf(bitmapOriginal.getHeight()));
+        bitmapTraite.setPixels(tabPixelsTraitement, 0, largeur, 0, 0, largeur,hauteur);
         this.iv.setImageBitmap(bitmapTraite);
     }
 
     public void sepia() {
 
-        int couleurPixel[] = new int[largeur * hauteur];
-
         int rouge;
         int vert;
         int bleu;
 
-        bitmapOriginal.getPixels(couleurPixel,0, largeur, 0, 0, largeur, hauteur);
 
-        for (int i = 0; i < couleurPixel.length; ++i) {
+        for (int i = 0; i < tabPixelsOriginal.length; ++i) {
 
-                rouge = (int) ((Color.red(couleurPixel[i]) * .393) + (Color.green(couleurPixel[i]) * .769) + (Color.blue(couleurPixel[i]) * .189));
-                vert = (int) ((Color.red(couleurPixel[i]) * .349) + (Color.green(couleurPixel[i]) * .686) + (Color.blue(couleurPixel[i]) * .168));
-                bleu = (int) ((Color.red(couleurPixel[i]) * .272) + (Color.green(couleurPixel[i]) * .534) + (Color.blue(couleurPixel[i]) * .131));
+                rouge = (int) ((Color.red(tabPixelsOriginal[i]) * .393) + (Color.green(tabPixelsOriginal[i]) * .769) + (Color.blue(tabPixelsOriginal[i]) * .189));
+                vert = (int) ((Color.red(tabPixelsOriginal[i]) * .349) + (Color.green(tabPixelsOriginal[i]) * .686) + (Color.blue(tabPixelsOriginal[i]) * .168));
+                bleu = (int) ((Color.red(tabPixelsOriginal[i]) * .272) + (Color.green(tabPixelsOriginal[i]) * .534) + (Color.blue(tabPixelsOriginal[i]) * .131));
 
                 if (rouge > 255)
                     rouge = 255;
@@ -65,25 +88,22 @@ public class Traitements {
                 if (bleu > 255)
                     bleu = 255;
 
-            couleurPixel[i] = Color.rgb(rouge, vert, bleu);
+            tabPixelsTraitement[i] = Color.rgb(rouge, vert, bleu);
         }
-        bitmapTraite.setPixels(couleurPixel,0, largeur, 0, 0, largeur, hauteur);
+        bitmapTraite.setPixels(tabPixelsTraitement,0, largeur, 0, 0, largeur, hauteur);
         this.iv.setImageBitmap(bitmapTraite);
     }
 
     public void colorize(){
         float hsv[] = new float[3];
 
-        int pixelSource[] = new int[largeur * hauteur];
-        bitmapOriginal.getPixels(pixelSource, 0, largeur, 0, 0, largeur, hauteur);
+        for (int i = 0; i < tabPixelsOriginal.length; ++i ){
 
-        for (int i = 0; i < pixelSource.length; ++i ){
-
-            Color.colorToHSV(pixelSource[i], hsv);
+            Color.colorToHSV(tabPixelsOriginal[i], hsv);
             hsv[0] = (float) 120;
-            pixelSource[i] = Color.HSVToColor(hsv);
+            tabPixelsTraitement[i] = Color.HSVToColor(hsv);
         }
-        bitmapTraite.setPixels(pixelSource, 0, largeur, 0, 0, largeur, hauteur);
+        bitmapTraite.setPixels(tabPixelsTraitement, 0, largeur, 0, 0, largeur, hauteur);
         iv.setImageBitmap(bitmapTraite);
     }
 
@@ -96,15 +116,15 @@ public class Traitements {
         int couleurRef = Color.RED;
         int distanceMax = 200;
 
-        int pixelSource[] = new int[largeur * hauteur];
-        bitmapOriginal.getPixels(pixelSource, 0, largeur, 0, 0, largeur, hauteur);
-
-        for (int i = 0; i < pixelSource.length; ++i){
-            if (Utils.distance(couleurRef, pixelSource[i]) >= distanceMax)
-                pixelSource[i] = couleurVersNG(pixelSource[i]);
+        for (int i = 0; i < tabPixelsOriginal.length; ++i){
+            if (Utils.distance(couleurRef, tabPixelsOriginal[i]) >= distanceMax)
+                tabPixelsTraitement[i] = couleurVersNG(tabPixelsOriginal[i]);
+            else{
+                tabPixelsTraitement[i] = tabPixelsOriginal[i]; // REcopie des valeurs des pixels de base.
+            }
         }
 
-        bitmapTraite.setPixels(pixelSource, 0, largeur, 0, 0, largeur,hauteur);
+        bitmapTraite.setPixels(tabPixelsTraitement, 0, largeur, 0, 0, largeur,hauteur);
         this.iv.setImageBitmap(bitmapTraite);
     }
 
@@ -119,4 +139,67 @@ public class Traitements {
         return cM;
     }
 
+    public void extensionContrasteRGB(){
+
+        int couleur;
+
+        for (int i = 0; i < tabPixelsOriginal.length; ++i){
+
+            // Récupération dans la table de la valeur modifiée corespondante à la couleur de base.
+
+            couleur = Color.rgb(lut.getValeurRouge(Color.red(tabPixelsOriginal[i])), lut.getValeurVert(Color.green(tabPixelsOriginal[i])), lut.getValeurBleu(Color.blue(tabPixelsOriginal[i])));
+            tabPixelsTraitement[i] = couleur;
+
+        }
+
+        bitmapTraite.setPixels(tabPixelsTraitement, 0, largeur, 0, 0, largeur, hauteur);
+        iv.setImageBitmap(bitmapTraite);
+
+    }
+
+    public void egaHistoExtensionContrasteRGB(){
+
+        int couleurR;
+        int couleurG;
+        int couleurB;
+
+        int nbpixels = histo.getNbpixels();
+
+        for (int i = 0; i < tabPixelsOriginal.length; i++){
+
+            couleurR = ( histo.getNombreUtilisationCouleurR(Color.red(tabPixelsOriginal[i])) ) * 255 / nbpixels;
+            couleurG = ( histo.getNombreUtilisationCouleurG(Color.green(tabPixelsOriginal[i])) ) * 255 / nbpixels;
+            couleurB = ( histo.getNombreUtilisationCouleurB(Color.blue(tabPixelsOriginal[i])) ) * 255 / nbpixels;
+
+            tabPixelsTraitement[i] = Color.rgb(couleurR, couleurG, couleurB);
+
+        }
+        bitmapTraite.setPixels(tabPixelsTraitement, 0, largeur, 0, 0, largeur, hauteur);
+        iv.setImageBitmap(bitmapTraite);
+
+    }
+
+    public void MettreAJourTableauxPixels(Bitmap bitmap){
+
+        tabPixelsOriginal = null;
+        tabPixelsTraitement = null;
+
+        bitmapOriginal = Bitmap.createScaledBitmap(bitmap,1500, 1500, false);
+        bitmapTraite = null;
+
+        this.hauteur = bitmapOriginal.getHeight();
+        this.largeur = bitmapOriginal.getWidth();
+
+        tabPixelsOriginal = new int[largeur * hauteur];
+        tabPixelsTraitement = new int[largeur * hauteur];
+
+        bitmapTraite = Bitmap.createBitmap(largeur, hauteur, Bitmap.Config.ARGB_8888);
+
+        Log.i("l", String.valueOf(largeur));
+        Log.i("h", String.valueOf(hauteur));
+
+        bitmapOriginal.getPixels(tabPixelsOriginal, 0, largeur, 0, 0, largeur, hauteur);
+        iv.setImageBitmap(bitmapOriginal);
+
+    }
 }
