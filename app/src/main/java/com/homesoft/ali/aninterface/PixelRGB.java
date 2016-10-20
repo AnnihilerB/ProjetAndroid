2e5dc3b9f06fbb86d97e06ce9f032963ea24fdcd
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-10-20 17:30:58 +0200

    Classe créant un PixelRGB

---
+++ app/src/main/java/com/homesoft/ali/aninterface/PixelRGB.java
@@ -0,0 +1,38 @@
+package com.homesoft.ali.aninterface;
+
+import android.graphics.Color;
+
+/**
+ * Created by ali on 14/10/16.
+ */
+
+public class PixelRGB {
+
+    private int rouge;
+    private int vert;
+    private int bleu;
+
+    public void setRouge(int rouge) {
+        this.rouge = rouge;
+    }
+
+    public void setVert(int vert) {
+        this.vert = vert;
+    }
+
+    public void setBleu(int bleu) {
+        this.bleu = bleu;
+    }
+
+    public int getRouge() {
+        return rouge;
+    }
+
+    public int getVert() {
+        return vert;
+    }
+
+    public int getBleu() {
+        return bleu;
+    }
+}
