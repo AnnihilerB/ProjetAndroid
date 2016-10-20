73236a732bdee387b1296bd3e4fccfa536f94802
Ali <ali.cherifi@etu.u-bordeaux.fr>
2016-10-20 17:30:25 +0200

    Classe générant un histogramme RGB

---
+++ app/src/main/java/com/homesoft/ali/aninterface/histogramme/HistogrammeRGB.java
@@ -0,0 +1,40 @@
+package com.homesoft.ali.aninterface.histogramme;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.util.Log;
+
+/**
+ * Created by ali on 10/10/16.
+ */
+
+public class HistogrammeRGB extends HistogrammeAbstract implements  Histogramme {
+
+    int histogrammeRouge[];
+    int histogrammeVert[];
+    int histogrammeBleu[];
+
+    public HistogrammeRGB(){
+        histogrammeRouge = new int[NBCOULEURS];
+        histogrammeVert = new int[NBCOULEURS];
+        histogrammeBleu = new int[NBCOULEURS];
+    }
+
+    public HistogrammeRGB getHistogramme() {
+        return null;
+    }
+
+    public void genererHistogramme(Bitmap bitmap) {
+        int hauteur = bitmap.getHeight();
+        int largeur = bitmap.getWidth();
+
+        int tabPix[] = new int[largeur * hauteur];
+        bitmap.getPixels(tabPix, 0, largeur, 0, 0, largeur, hauteur);
+
+        for (int i = 0; i < tabPix.length; i++){
+            histogrammeRouge[Color.red(tabPix[i])] ++;
+            histogrammeVert[Color.green(tabPix[i])] ++;
+            histogrammeBleu[Color.blue(tabPix[i])] ++;
+        }
+    }
+}
